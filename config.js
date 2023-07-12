d4a439102de4048385d86f5d931bd95aad1e7048
youselene <123219794+youselene@users.noreply.github.com>
2023-07-12 16:38:33 -0500

    config.js

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
 <script>
-  // Verificar si el usuario ha iniciado sesiÛn
+  // Verificar si el usuario ha iniciado sesi√≥n
   function checkSession() {
-    // Verificar si la cookie o el valor de almacenamiento local est· presente y es v·lido
-    // AquÌ asumimos que est·s utilizando cookies como mÈtodo de almacenamiento
+    // Verificar si la cookie o el valor de almacenamiento local est√° presente y es v√°lido
+    // Aqu√≠ asumimos que est√°s utilizando cookies como m√©todo de almacenamiento
     const isLoggedIn = document.cookie.includes('isLoggedIn=true');
     const isAuthenticated = localStorage.getItem('authenticated') === 'true';
 
-    // Si el usuario no ha iniciado sesiÛn, redirigirlo a la p·gina de inicio de sesiÛn
+    // Si el usuario no ha iniciado sesi√≥n, redirigirlo a la p√°gina de inicio de sesi√≥n
     if (!isLoggedIn && !isAuthenticated) {
       window.location.href = 'https://www.youselene.github.io/buscador/';
     }
   }
 
-  // Llamar a la funciÛn de verificaciÛn de sesiÛn al cargar la p·gina
+  // Llamar a la funci√≥n de verificaci√≥n de sesi√≥n al cargar la p√°gina
   checkSession();
 
-  // FunciÛn para realizar la b˙squeda en Google Sheets
+  // Funci√≥n para realizar la b√∫squeda en Google Sheets
   function searchInGoogleSheets(searchTerm) {
-    // ObtÈn solo los n˙meros de la URL utilizando una expresiÛn regular
+    // Obt√©n solo los n√∫meros de la URL utilizando una expresi√≥n regular
     var numbers = searchTerm.match(/\d{5,}/g);
 
-    // Verifica si se encontraron n˙meros en el tÈrmino de b˙squeda
+    // Verifica si se encontraron n√∫meros en el t√©rmino de b√∫squeda
     if (numbers !== null) {
-      // Obtiene el primer n˙mero encontrado
+      // Obtiene el primer n√∫mero encontrado
       var number = numbers[0];
 
-      // ID de la hoja de c·lculo de Google Sheets
+      // ID de la hoja de c√°lculo de Google Sheets
       var spreadsheetId = '1uxwfQA1Dj7XHechUB2GWaYhq-VPl2rMzPepNSE3zDtM';
 
-      // Nombre de la hoja de c·lculo en la que se realizar· la b˙squeda
+      // Nombre de la hoja de c√°lculo en la que se realizar√° la b√∫squeda
       var sheetName = 'shutterstock';
 
-      // Credenciales de autenticaciÛn de la API de Google Sheets
+      // Credenciales de autenticaci√≥n de la API de Google Sheets
       var apiKey = 'AIzaSyDCWMQksGjZpej-HmgTNe2fP_HQp4og0Bc'; // Reemplaza con tu propia API Key
 
       // Construir la URL de la solicitud a la API de Google Sheets
       var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`;
 
-      // Realizar la solicitud GET para obtener los datos de la hoja de c·lculo
+      // Realizar la solicitud GET para obtener los datos de la hoja de c√°lculo
       fetch(url)
         .then(function(response) {
           return response.json();
@@ -46,14 +46,14 @@
           var rows = data.values;
           var results = [];
 
-          // Recorrer las filas y buscar el n˙mero en la columna A
+          // Recorrer las filas y buscar el n√∫mero en la columna A
           for (var i = 0; i < rows.length; i++) {
           if (rows[i][0] && rows[i][0] === number) {
             results.push(rows[i][1]); // Agregar el valor de la columna B al resultado
           }
         }
 
-          // Mostrar los resultados en la p·gina web
+          // Mostrar los resultados en la p√°gina web
           var resultsContainer = document.querySelector('.search-results');
           resultsContainer.innerHTML = '';
 
@@ -62,7 +62,7 @@
               var resultLink = document.createElement('a');
               resultLink.href = results[k]; // Establecer la URL como el resultado
               resultLink.textContent = results[k]; // Establecer el texto del enlace como el resultado
-              resultLink.target = '_blank'; // Abrir enlace en una nueva pestaÒa
+              resultLink.target = '_blank'; // Abrir enlace en una nueva pesta√±a
               resultLink.rel = 'noopener noreferrer'; // Configurar atributos de seguridad para enlaces externos
               resultsContainer.appendChild(resultLink);
               resultsContainer.appendChild(document.createElement('br'));
@@ -77,7 +77,7 @@
           console.log('Error al obtener los datos de Google Sheets:', error);
         });
     } else {
-      // No se encontraron n˙meros en el tÈrmino de b˙squeda
+      // No se encontraron n√∫meros en el t√©rmino de b√∫squeda
       var resultsContainer = document.querySelector('.search-results');
       resultsContainer.innerHTML = '';
 
@@ -87,15 +87,15 @@
     }
   }
 
-  // Obtener el formulario de b˙squeda
+  // Obtener el formulario de b√∫squeda
   var searchForm = document.querySelector('.search-form');
 
-     // Agregar un evento de envÌo de formulario
+     // Agregar un evento de env√≠o de formulario
       searchForm.addEventListener('submit', function(event) {
         event.preventDefault();
         var searchTerm = searchForm.querySelector('input[name="search-term"]').value;
 
-        // Validar si se ingresÛ un tÈrmino de b˙squeda
+        // Validar si se ingres√≥ un t√©rmino de b√∫squeda
         if (searchTerm.trim() !== '') {
           searchInGoogleSheets(searchTerm);
         } else {
@@ -103,7 +103,7 @@
           resultsContainer.innerHTML = '';
 
           var noResultsMessage = document.createElement('p');
-          noResultsMessage.textContent = 'Por favor, ingrese un tÈrmino de b˙squeda.';
+          noResultsMessage.textContent = 'Por favor, ingrese un t√©rmino de b√∫squeda.';
           resultsContainer.appendChild(noResultsMessage);
         }
       });
